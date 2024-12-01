55be7c7f96109263aae3d9f6a90195bdfe81a6d0
leandro <leandro.rodrigues00@hotmail.com>
2024-12-01 11:11:07 -0300

    feat: add prism repository, and connecting the infra layer

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,25 +1,18 @@
+import { InMemoryNotificationRepository } from '../../../test/repositories/in-memory-notifications-repository';
 import { SendNotification } from './send-notification';
-import { Notification } from '../entities/notification';
-
-const notifications: Notification[] = [];
-
-const NotificationRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification(NotificationRepository);
+    const notificationsRepository = new InMemoryNotificationRepository();
+    const sendNotification = new SendNotification(notificationsRepository);
 
-    await sendNotification.execute({
+    const { notification } = await sendNotification.execute({
       content: 'This is a notification',
       category: 'social',
       recipientId: 'example-recipient-id',
     });
 
-    console.log(notifications);
-    expect(notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
